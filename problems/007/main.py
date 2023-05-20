0dea1e82f18b55675c727e5e81be7e7845ce135b
jarredblanchette <jarred.blanchette@gmail.com>
2023-05-20 16:52:32 -0400

    12 was fun, trick was realising that all numbers are products of prime factors, so you can work out the number of factors by just getting the prime roots and working out how many combinations there are of those roots. Fixed a bug in the sieve where would crash if no roots in numbers_to_sieve.

--- problems/007/main.py
+++ problems/007/main.py
@@ -3,15 +3,19 @@
 # What is the 10 001st prime number?
 
 def sieve(primes, numbers_to_sieve):
+    """Sieves out the non-prime numbers
+        :param primes: A set of known primes to act as a seed
+        :param numbers_to_sieve: the set of numbers we're going to filter.  If primes and numbers_to_sieve abut, we are gauranteed to have only primes in numbers_to_sieve
+        :returns list of prime numbers and the prime numbers in the numbers_to_sieve"""
     numbers_to_sieve = list(numbers_to_sieve)
 
-    #my kingdom for the filter method to work on filter objects.
+    # my kingdom for the filter method to work on filter objects.
     for prime in primes + numbers_to_sieve:
-        if prime > max(numbers_to_sieve)/2:
+        if prime > max(primes + numbers_to_sieve) / 2 or len(numbers_to_sieve) == 0:
             break
         numbers_to_sieve = list(filter(lambda x: x % prime != 0 or x == prime, numbers_to_sieve))
 
-    return primes,numbers_to_sieve
+    return primes, numbers_to_sieve
 
 
 if __name__ == '__main__':
